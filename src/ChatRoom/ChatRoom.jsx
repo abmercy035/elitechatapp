bfdc84660cb7f08d9643392c881c56e0b82279b0
Javascript Enthusiast <abmercy035@gmail.com>
2022-10-07 12:55:19 +0100

    make it better

--- src/ChatRoom/ChatRoom.jsx
+++ src/ChatRoom/ChatRoom.jsx
@@ -14,33 +14,20 @@ const ChatRoom = ({ room }) => {
      setNewMessage(event.target.value);
   };
 
-  window.onbeforeunload = (event) => {
-    const e = event || window.event;
-    // Cancel the event
-    console.log(e)
-    e.preventDefault();
-    if (e) {
-      // Legacy method for cross browser support
-      // e.returnValue = 'do yoi?';
-      navigate("/");
-    }
-    navigate("/");
-    // return ''; // Legacy method for cross browser support
-  };
+  // window.onbeforeunload = (event) => {
+  //   const e = event || window.event;
+  //   // Cancel the event
+  //   console.log(e)
+  //   e.preventDefault();
+  //   if (e) {
+  //     // Legacy method for cross browser support
+  //     // e.returnValue = 'do yoi?';
+  //     navigate("/");
+  //   }
+  //   navigate("/");
+  //   // return ''; // Legacy method for cross browser support
+  // };
 
-  window.onafterload = (event) => {
-    const e = event || window.event;
-    // Cancel the event
-    console.log(e)
-    // e.preventDefault();
-    if (e) {
-      // Legacy method for cross browser support
-      // e.returnValue = '';
-      navigate("/");
-    }
-    navigate("/");
-    // return ''; // Legacy method for cross browser support
-  };
   useEffect(() => {
     if (!roomId) navigate("/");
     inputEl.current.scrollIntoView({ behavior: "smooth" });
@@ -93,7 +80,7 @@ const ChatRoom = ({ room }) => {
           className="new-message-input-field"
         />
         <button
-          onClick={ () => newMessage && handleSendMessage}
+          onClick={()=>{newMessage && handleSendMessage()}}
           className="send-message-button"
         >
           Send

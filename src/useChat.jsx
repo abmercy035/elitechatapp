429e8be8a5eb6045ef419c765177dae8946ec4a0
Javascript Enthusiast <abmercy035@gmail.com>
2022-10-07 12:58:21 +0100

    make it better

--- src/useChat.jsx
+++ src/useChat.jsx
@@ -2,7 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-const SOCKET_SERVER_URL = "https://elitechatapi.herokuapp.com";
+// const SOCKET_SERVER_URL = "https://elitechatapi.herokuapp.com";
+const SOCKET_SERVER_URL = "http://localhost:5000";
 
 const useChat = (roomId) => {
   const [messages, setMessages] = useState([]);
